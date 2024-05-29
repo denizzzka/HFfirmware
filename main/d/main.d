7553145df3693506c023c31a119349444f8009d7
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-05-29 16:15:58 +0300

    import i2c_master added, but .i file isn't suitable version for D compiler

--- main/d/main.d
+++ main/d/main.d
@@ -1,8 +1,15 @@
 module main;
 
+import i2c_master;
+
+pragma(crt_constructor)
+shared static this()
+{
+}
+
 extern(C) void app_main()
 {
     while(1) {
-
+        vTaskDelay(portTICK_PERIOD_MS * 10);
     }
 }
