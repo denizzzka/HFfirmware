b95bb472b6fcb2f7f7ba8e1b807265906d3425b2
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-05-29 14:44:47 +0300

    Renaming *.c.i -> *.i added

--- build.sh
+++ build.sh
@@ -8,5 +8,8 @@ set -euox pipefail
 # First stage: preprocess *.h to *.i files
 PRESERVE_I_FILES=y idf.py --build-dir=preprocessed set-target esp32c3 build
 
+# Mass rename *.c.i files to *.i to satisfy D compiler
+mmv -r './preprocessed/;*.c.i' '#2.i'
+
 # Second stage: build and link with D files
 idf.py --build-dir=builddir set-target esp32c3 build
