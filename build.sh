623587e362a3603cb503380bae51a7217db253ab
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-05-30 00:32:20 +0300

    removes "# " lines, adds -include prepr.h

--- build.sh
+++ build.sh
@@ -11,9 +11,11 @@ PRESERVE_I_FILES=y idf.py --build-dir=preprocessed set-target esp32c3 build
 # Mass rename *.c.i files to *.i (GCC produces *.c.i files for unknown reason)
 mmv -r './preprocessed/;*.c.i' '#2.i'
 
-# Preprocess *.i files applying importc.h to satisfy D compiler
+# Preprocess *.i files applying importc.h and prepr.h to satisfy D compiler
+# Also removes "# " lines to avoid using additional .h/.c files
+PREPR_PATH=$(dirname "$(realpath $0)")/prepr.h
 PREP_FLAGS="-dD -Wno-builtin-macro-redefined -x c -E -include /home/denizzz/ldc2_standalone/import/importc.h"
-fdfind --base-directory ./preprocessed/ --type f --glob *.i --exec clang $PREP_FLAGS -o ./'{//}'/'{/.}'.i '{}' \;
+fdfind --base-directory ./preprocessed/ --type f --glob *.i --exec clang $PREP_FLAGS -include "${PREPR_PATH}" -o ./'{//}'/'{/.}'.i '{}' \; --exec sed -i -r 's/^# .+//g' {} \;
 
 # Second stage: build and link with D files
 idf.py --build-dir=builddir set-target esp32c3 build
---
+++ prepr.h
@@ -0,0 +1,2 @@
+#define _Static_assert(expr) static_assert(expr, "_Static_assert message undefined")
+//~ #define _Static_assert(expr, msg) static_assert(expr, msg)
