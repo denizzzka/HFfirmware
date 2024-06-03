9532836b99fd38bc03906e2bfb1d06866bee2876
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-06-03 13:30:40 +0300

    echo Processing *.i files... added

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ set -euox pipefail
 # First stage: preprocess *.h to *.i files
 PRESERVE_I_FILES=y idf.py --build-dir=preprocessed set-target esp32c3 build
 
+echo Processing *.i files...
+
 # Mass rename *.c.i files to *.i (GCC produces *.c.i files for unknown reason)
 mmv -r './preprocessed/;*.c.i' '#2.i'
 
@@ -16,6 +18,8 @@ mmv -r './preprocessed/;*.c.i' '#2.i'
 PREPR_PATH=$(dirname "$(realpath $0)")/prepr.h
 PREP_FLAGS="-dD -Wno-builtin-macro-redefined -x c -E -include /home/denizzz/ldc2_standalone/import/importc.h"
 fdfind --base-directory ./preprocessed/ --type f --glob *.i --exec clang $PREP_FLAGS -include "${PREPR_PATH}" -o ./'{//}'/'{/.}'.i '{}' \; --exec sed -i -r 's/^# .+//g' {} \;
+
+echo Processing *.i files done
 
 # Second stage: build and link with D files
 idf.py --build-dir=builddir set-target esp32c3 build
