7884b7227513e4c951ea409344fed064db054634
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-06-07 23:03:33 +0300

    Bindings build script updated

--- build.sh
+++ build.sh
@@ -8,20 +8,22 @@ set -euox pipefail
 # First stage: preprocess *.h to *.i files
 PRESERVE_I_FILES=y idf.py --build-dir=preprocessed set-target esp32c3 build
 
-echo Processing *.i files...
+echo "Processing *.i files..."
 
 # Removes definitions from .i files. For this purpose we need create intermediate conventional .c files from .i files
 # Also removes "# " lines to avoid makeheaders tool errors
-fdfind --base-directory ./preprocessed/esp-idf --type f --size +1b --glob "*.c.i" --ignore-file ../../fd_ignore.txt \
+# restrict in freertos isn't compatible with __restrict in esp_ringbuf, ESP IDF issue?
+fdfind --base-directory ./preprocessed/esp-idf --type f --glob "*.c.i" --ignore-file ../../fd_ignore.txt \
     --exec cp {} {.} \; \
-    --exec sed -i -r 's/^# .+//g' {.} \; \
-    --exec sed -i -r 's/asm volatile/__asm volatile/g' {.} \;
+    --exec sed -i -r 's/asm volatile/__asm volatile/g' {.} \; \
+    --exec sed -i -r 's/__restrict/restrict/g' {.} \; \
+    --exec echo ./preprocessed/esp-idf/{.} \; | ~/Dev/diprocessor/diprocessor > ./preprocessed/processed_for_dpp.c
 
 # Create D bindings from generated .c files
 # FIXME: Used DPP branch: https://github.com/denizzzka/dpp/tree/c_and_i_files
-~/Dev/dpp/bin/d++ --preprocess-only --no-sys-headers --include-path=./preprocessed/esp-idf/ --source-output-path=./preprocessed/ esp_idf.dpp
+~/Dev/dpp/bin/d++ --preprocess-only --no-sys-headers --include-path=./preprocessed/ --source-output-path=./preprocessed/ esp_idf.dpp
 
-echo Processing *.i files done
+echo "Processing *.i files done"
 
 # Second stage: build and link with D files
 idf.py --build-dir=builddir set-target esp32c3 build
